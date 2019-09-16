a2fe16530bc55e14b592a92424b6f87804d23ae3
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-09-16 14:32:11 -0700

    dashboard test w/ green & red led cmplte

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -11,9 +11,21 @@ describe('<Display />', () => {
       const display = render(<Display closed={false} locked={false} />);
       const unlocked = display.getByText(/unlock/i);
       const open = display.getByText(/open/i);
-      display.queryByText(/closed/i);
+    //   display.queryByText(/closed/i);
       expect(unlocked).toHaveClass('green-led');
       expect(open).toHaveClass('green-led');
 
+    });
+    it('closed and unlocked', () => {
+        const { getByText } = render(<Display closed={true} locked={false} />);
+        getByText(/unlocked/i);
+        getByText(/closed/i);
+    });
+    it('closed and locked', () => {
+        const { getByText } = render(<Display closed={true} locked={true} />);
+        const locked = getByText(/locked/i);
+        const closed = getByText(/closed/i);
+        expect(locked).toHaveClass('red-led');
+        expect(closed).toHaveClass('red-led');
     });
 });
