7c64bb17d779e7bc23110351a0768a065631e94e
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-09-16 13:50:43 -0700

    commit 2

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Display from '../display/Display';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('<Display />', () => {
     it('renders without crashing', () => {
         render(<Display />);
+    });
+    it('open and unlock', () => {
+      const display = render(<Display closed={false} locked={false} />);
+      const unlocked = display.getByText(/unlock/i);
+      const open = display.getByText(/open/i);
+      display.queryByText(/closed/i);
+      expect(unlocked).toHaveClass('green-led');
+      expect(open).toHaveClass('green-led');
+
     });
 });
