9d85bf3713f5fb767a9049735d0113b418909089
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-09-16 13:11:18 -0700

    commit 1

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
-// Test away
+import React from 'react';
+import Display from '../display/Display';
+import { render } from '@testing-library/react';
+
+describe('<Display />', () => {
+    it('renders without crashing', () => {
+        render(<Display />);
+    });
+});
