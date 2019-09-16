1c095927004dfc457cf885b71398aff486913e85
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-09-16 15:09:18 -0700

    first test in controls working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
-// Test away!
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Controls from '../controls/Controls';
+
+
+afterEach(cleanup);
+describe('<Controls />', () => {
+    it('renders without crashing', () => {
+        render(<Controls />);
+    });
+    it('open and unlocked', () => {
+        const Michael = render(<Controls closed={false} locked={false}/>);
+        const closeBtn = Michael.getByText(/close gate/i);
+        const lockBtn = Michael.getByText(/lock gate/i); 
+        expect(closeBtn.disabled).toBeFalsy();
+        expect(lockBtn.disabled).toBeTruthy();
+    })
+});
