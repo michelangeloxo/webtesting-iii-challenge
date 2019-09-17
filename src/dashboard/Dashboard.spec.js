837284df55c524855450ffe16bd50478be7b1f7d
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-09-16 21:25:43 -0700

    mvp complete i think

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import Display from '../display/Display';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard';
 
-describe('<Display />', () => {
+describe('<Dashboard', () => {
     it('renders without crashing', () => {
-        render(<Display />);
+        render(<Dashboard />);
     });
-    it('open and unlock', () => {
-      const display = render(<Display closed={false} locked={false} />);
-      const unlocked = display.getByText(/unlock/i);
-      const open = display.getByText(/open/i);
-    //   display.queryByText(/closed/i);
-      expect(unlocked).toHaveClass('green-led');
-      expect(open).toHaveClass('green-led');
+    it('default state open and unlocked', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText(/^open$/i);
+        getByText(/^unlocked$/i);
 
-    });
-    it('closed and unlocked', () => {
-        const { getByText } = render(<Display closed={true} locked={false} />);
-        getByText(/unlocked/i);
-        getByText(/closed/i);
-    });
-    it('closed and locked', () => {
-        const { getByText } = render(<Display closed={true} locked={true} />);
-        const locked = getByText(/locked/i);
-        const closed = getByText(/closed/i);
-        expect(locked).toHaveClass('red-led');
-        expect(closed).toHaveClass('red-led');
-    });
+        const lockBtn = getByText(/^lock gate$/i);
+        const closeBtn = getByText(/^close gate$/i);
+   });
+
+
+
+    
 });
